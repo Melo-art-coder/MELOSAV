611004e9ed5967c39cde011988bcc700949d5d91
Melo-art-coder <MelodyKamto@gmail.com>
2026-07-14 22:15:00 +0100

    Update transfer.js

--- transfer.js
+++ transfer.js
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (continueBtn) {
 
         continueBtn.addEventListener("click", () => {
-
+console.log("CONTINUE BUTTON CLICKED");
             const bank =
             document.getElementById("transferBank").value;
 
