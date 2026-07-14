b9cde15f9e3c7e64a0124d1f9431a8d621fb0c9a
Melo-art-coder <MelodyKamto@gmail.com>
2026-07-14 15:54:56 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "melosave-cache-v2";
+const CACHE_NAME = "melosave-cache-v3";
+
 const assets = [
   "/MELOSAV/",
   "/MELOSAV/index.html",
@@ -6,26 +7,77 @@ const assets = [
   "/MELOSAV/login.html",
   "/MELOSAV/signup.html",
   "/MELOSAV/profile.html",
+
   "/MELOSAV/style.css",
-  "/MELOSAV/script.js",
+  "/MELOSAV/home.css",
+  "/MELOSAV/dashboard-v3.css",
+
+  "/MELOSAV/storage.js",
+  "/MELOSAV/profile.js",
+  "/MELOSAV/notifications.js",
+  "/MELOSAV/welcome.js",
+  "/MELOSAV/theme.js",
+  "/MELOSAV/auth.js",
+  "/MELOSAV/home.js",
+  "/MELOSAV/transactions.js",
+  "/MELOSAV/budget.js",
+
   "/MELOSAV/icon-192.png",
   "/MELOSAV/icon-512.png"
 ];
 
-// Install Service Worker and cache assets
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      cache.addAll(assets);
-    })
+
+// Install
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(cache => cache.addAll(assets))
   );
+
 });
 
-// Fetch assets from cache when offline
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request);
+
+// Delete old cache
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys.map(key => {
+
+          if(key !== CACHE_NAME){
+
+            return caches.delete(key);
+
+          }
+
+        })
+
+      );
+
     })
+
   );
+
+});
+
+
+// Fetch
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+    .then(response => {
+
+      return response || fetch(event.request);
+
+    })
+
+  );
+
 });
